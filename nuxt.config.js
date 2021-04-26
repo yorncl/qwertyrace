78d8973ac4876e0ed7a1034c1af17b728d679fc7
yorn <yornclaudel@gmail.com>
2021-04-27 00:48:14 +0200

    adding a script for github pages deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
+    // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'qwertyrun',
     htmlAttrs: {
