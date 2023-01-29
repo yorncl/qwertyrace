f7cfecf289207c1a6629835b24edc9da855a7e54
yorncl <32569804+yorncl@users.noreply.github.com>
2023-01-29 11:12:48 +0000

    Deploying to gh-pages from  @ bc063b467041f43f0f03298a1fe6ddbbc8acb092 🚀

--- _nuxt/static/1619552587/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fqwertyrun\u002F_nuxt\u002Fstatic\u002F1619552587",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fqwertyrun\u002F",assetsPath:"\u002Fqwertyrun\u002F_nuxt\u002F",cdnURL:a}}}}(null));
